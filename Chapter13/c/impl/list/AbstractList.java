89bdbce1bca6d5181121a2cd25f388ff5068104d
Sejin Park <sejinpark@Sejins-MacBook-Pro.local>
2023-08-03 05:26:41 +0900

    Volume 1

---
+++ Chapter13/c/impl/list/AbstractList.java
@@ -0,0 +1,6 @@
+package lang.thegodofjava.Chapter13.c.impl.list;
+
+public abstract class AbstractList implements List{
+
+    public abstract void clear();
+}
---
+++ Chapter13/c/impl/list/List.java
@@ -0,0 +1,10 @@
+package lang.thegodofjava.Chapter13.c.impl.list;
+
+public interface List {
+
+    public void add();
+
+    public void update(int index, Object value);
+
+    public void remove(int index);
+}
